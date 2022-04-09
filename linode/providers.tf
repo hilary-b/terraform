469db0127e5434bbece9664aeab35b3877e71efe
hilary-b <hilary.brennan@nextechar.com>
2022-04-09 12:11:45 -0400

    initial linode files

--- linode/providers.tf
+++ linode/providers.tf
@@ -1,7 +1,18 @@
 terraform {
   required_providers {
     linode = {
-      source  = "linode/linode"
+      source                    = "linode/linode"
     }
+  }
+
+
+  backend "s3" {
+    profile                     = "linode-s3"
+    bucket                      = "tf-backend"
+    key                         = "tfstate.json"
+    region                      = "us-east-1"
+    endpoint                    = "us-east-1.linodeobjects.com"
+    skip_credentials_validation = true
+    # access_key and secrey_key stored locally
   }
 }
