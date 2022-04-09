9161d5e11bace8b5b2356d4f8b4dbb1d6c99062c
hilary-b <hilary.brennan@nextechar.com>
2022-04-09 12:12:15 -0400

    remove the remote backend for now and fuck aorund with it later

--- linode/providers.tf
+++ linode/providers.tf
@@ -6,13 +6,4 @@ terraform {
   }
 
 
-  backend "s3" {
-    profile                     = "linode-s3"
-    bucket                      = "tf-backend"
-    key                         = "tfstate.json"
-    region                      = "us-east-1"
-    endpoint                    = "us-east-1.linodeobjects.com"
-    skip_credentials_validation = true
-    # access_key and secrey_key stored locally
-  }
 }
